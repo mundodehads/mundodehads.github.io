f61d77aa21a40093be0d0d5a1272fe8408d31749
mundodehads <omundodehads@gmail.com>
2023-08-29 17:01:20 -0300

    Add generador de bingo v1 code from Talyson

--- geradordebingo/index.js
+++ geradordebingo/index.js
@@ -1,31 +1,15 @@
-function gerarCSV(cartelas){
-
-    let csv = ""
-    
-    for(var i=0; i< cartelas.length; i++){
-        var numCartelas = cartelas[i].split(",")
-        csv += "<tr><td>"+(i+1)+"</td><td>"+numCartelas[0]+"</td><td>"+numCartelas[1]+"</td><td>"+numCartelas[2]+"</td><td>"+numCartelas[3]+"</td><td>"+numCartelas[4]+"</td><td>"+numCartelas[5]+"</td><td>"+numCartelas[6]+"</td><td>"+numCartelas[7]+"</td><td>"+numCartelas[8]+"</td><td>"+numCartelas[9]+"</td><td>"+numCartelas[10]+"</td><td>"+numCartelas[11]+"</td><td>"+numCartelas[12]+"</td><td>"+numCartelas[13]+"</td><td>"+numCartelas[14]+"</td><td>"+numCartelas[15]+"</td><td>"+numCartelas[16]+"</td><td>"+numCartelas[17]+"</td><td>"+numCartelas[18]+"</td><td>"+numCartelas[19]+"</td><td>"+numCartelas[20]+"</td><td>"+numCartelas[21]+"</td><td>"+numCartelas[22]+"</td><td>"+numCartelas[23]+"</td><td>"+numCartelas[24]+"</td></tr>";                          
-    }
-
-    console.log(csv)
-    alert(csv)
-}
-
-
 function gerarBloco(numCartelas){
-    const cartelas = new Array(numCartelas);
-    cartelas[0]= gerarCartela();
-
-    for (let index = 1; index < numCartelas; index++) {
+	const cartelasCsv = [['Numero','N1','N2','N3','N4','N5','N6','N7','N8','N9','N10','N11','N12','N13','N14','N15','N16','N17','N18','N19','N20','N21','N22','N23','N24','N25']]
+    	
+    for (let index = 1; index <= numCartelas; index++) {
         const cartela = gerarCartela();
-        if(cartelas.indexOf(cartela) != -1){  
+        if(cartelasCsv.indexOf(cartela) != -1){  
             index--;
         }else{
-            cartelas[index] = cartela;
+			cartelasCsv.push([index, ...cartela.split(',')]);
         }
     }
-
-    return cartelas;
+    return cartelasCsv;
 }
 
 function gerarCartela () { 
@@ -39,12 +23,14 @@ function gerarCartela () {
     gerarNumeros(31,46,matriz_N);
     gerarNumeros(46,61,matriz_G);
     gerarNumeros(61,76,matriz_O);
+	
     const cartela= matriz_B +","+ matriz_I +","+ matriz_N +","+ matriz_G +","+ matriz_O;
     return cartela;
  }
 
 function gerarNumeros(min,max,matriz){
     let cont=0;
+
     do {
         const num = getRndInteger(min,max);
         if(matriz.indexOf(num) != -1){  
@@ -54,6 +40,7 @@ function gerarNumeros(min,max,matriz){
             cont++;
         }
     } while (cont <= 4);
+
     matriz.sort(compararNumeros);
 }
 
@@ -68,9 +55,52 @@ function compararNumeros(a, b) {
 
 function onGenerateClick() {
     const quantity = document.getElementById('quantity').value;
-    //  const maxnumhouses = document.getElementById('maxnumhouses').value;
-
     const cartelas = gerarBloco(quantity);
-    gerarCSV(cartelas);
+
+	exportToCsv('export.csv', cartelas)
 }
+
+function exportToCsv(filename, rows) {
+	var processRow = function (row) {
+		var finalVal = '';
+		for (var j = 0; j < row.length; j++) {
+			var innerValue = row[j] === null ? '' : row[j].toString();
+			if (row[j] instanceof Date) {
+				innerValue = row[j].toLocaleString();
+			};
+			var result = innerValue.replace(/"/g, '""');
+			if (result.search(/("|,|\n)/g) >= 0)
+				result = '"' + result + '"';
+			if (j > 0)
+				finalVal += ',';
+			finalVal += result;
+		}
+		return finalVal + '\n';
+	};
+
+	var csvFile = '';
+	for (var i = 0; i < rows.length; i++) {
+		csvFile += processRow(rows[i]);
+	}
+
+	var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
+	if (navigator.msSaveBlob) {
+		navigator.msSaveBlob(blob, filename);
+	} else {
+		var link = document.createElement("a");
+		if (link.download !== undefined) {
+			var url = URL.createObjectURL(blob);
+			link.setAttribute("href", url);
+			link.setAttribute("download", filename);
+			link.style.visibility = 'hidden';
+			document.body.appendChild(link);
+			link.click();
+			document.body.removeChild(link);
+		}
+	}
+}
+   
+    
+    
+
 
