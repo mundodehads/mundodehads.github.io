c47d4b54c380b2a2d6da0a2e41840a56ef982332
mundodehads <omundodehads@gmail.com>
2023-08-29 17:09:33 -0300

    Change CSV to RTF

--- geradordebingo/index.js
+++ geradordebingo/index.js
@@ -1,12 +1,12 @@
 function gerarBloco(numCartelas){
-	const cartelasCsv = [['Numero','N1','N2','N3','N4','N5','N6','N7','N8','N9','N10','N11','N12','N13','N14','N15','N16','N17','N18','N19','N20','N21','N22','N23','N24','N25']]
+	const cartelasCsv = [['\\Numero','N1','N2','N3','N4','N5','N6','N7','N8','N9','N10','N11','N12','N13','N14','N15','N16','N17','N18','N19','N20','N21','N22','N23','N24','N25\\']]
     	
     for (let index = 1; index <= numCartelas; index++) {
         const cartela = gerarCartela();
         if(cartelasCsv.indexOf(cartela) != -1){  
             index--;
         }else{
-			cartelasCsv.push([index, ...cartela.split(',')]);
+			cartelasCsv.push([`\\${index}`, ...cartela.split(',')]);
         }
     }
     return cartelasCsv;
@@ -24,7 +24,7 @@ function gerarCartela () {
     gerarNumeros(46,61,matriz_G);
     gerarNumeros(61,76,matriz_O);
 	
-    const cartela= matriz_B +","+ matriz_I +","+ matriz_N +","+ matriz_G +","+ matriz_O;
+    const cartela= matriz_B +","+ matriz_I +","+ matriz_N +","+ matriz_G +","+ matriz_O+"\\";
     return cartela;
  }
 
@@ -57,10 +57,10 @@ function onGenerateClick() {
     const quantity = document.getElementById('quantity').value;
     const cartelas = gerarBloco(quantity);
 
-	exportToCsv('export.csv', cartelas)
+	exportToRtf('export.rtf', cartelas)
 }
 
-function exportToCsv(filename, rows) {
+function exportToRtf(filename, rows) {
 	var processRow = function (row) {
 		var finalVal = '';
 		for (var j = 0; j < row.length; j++) {
@@ -72,7 +72,7 @@ function exportToCsv(filename, rows) {
 			if (result.search(/("|,|\n)/g) >= 0)
 				result = '"' + result + '"';
 			if (j > 0)
-				finalVal += ',';
+				finalVal += '\\\\';
 			finalVal += result;
 		}
 		return finalVal + '\n';
@@ -83,7 +83,7 @@ function exportToCsv(filename, rows) {
 		csvFile += processRow(rows[i]);
 	}
 
-	var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
+	var blob = new Blob([csvFile], { type: 'text/rtf;charset=utf-8;' });
 	if (navigator.msSaveBlob) {
 		navigator.msSaveBlob(blob, filename);
 	} else {
